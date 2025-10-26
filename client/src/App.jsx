143084a754343fe4c0ab8063ad7b52174325b96b
Jose Alvarez <rosney.sotor@gmail.com>
2025-10-26 18:55:22 -0300

    Se modifica archivo app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,26 +2,27 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.jsx";
 import CustomerList from "./pages/CustomerPage.jsx";
-// import CreateCustomerPage from "./pages/CreateCustomerPage.jsx";
-import DashboardLayout from "./components/DashboardLayaout.jsx"; // Nuevo componente para el layout
-import OrderBuyCard from "./pages/OrderBuyPage.jsx"
+import DashboardLayout from "./components/DashboardLayaout.jsx";
+import OrderBuyCard from "./pages/OrderBuyPage.jsx";
 
 export default function App() {
    return (
-      <Router>
+      <Router basename="/CRM-Beta">
          <Routes>
             {/* Ruta de Login */}
             <Route path="/login" element={<LoginPage />} />
 
-            {/* Rutas Protegidas con Sidebar */}
-            <Route path="/" element={<DashboardLayout />}>
+            {/* Redirección desde la raíz hacia el login */}
+            <Route path="/" element={<Navigate to="/login" replace />} />
+
+            {/* Rutas protegidas */}
+            <Route path="/dashboard" element={<DashboardLayout />}>
                <Route path="customer" element={<CustomerList />} />
-               {/* <Route path="customer/view-add" element={<CreateCustomerPage />} /> */}
                <Route path="order" element={<OrderBuyCard />} />
             </Route>
 
-            {/* Redirección si la ruta no existe */}
-            <Route path="*" element={<Navigate to="/customer" />} />
+            {/* Si no existe la ruta, mandamos al login */}
+            <Route path="*" element={<Navigate to="/login" replace />} />
          </Routes>
       </Router>
    );
