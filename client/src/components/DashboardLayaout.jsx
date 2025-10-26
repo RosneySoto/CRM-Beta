8b317a1d8eb57777ca04ac3543b0c0ef5c253754
Jose Alvarez <rosney.sotor@gmail.com>
2025-10-25 23:29:32 -0300

    Se modifica js-cookie por jsonwebtoken

--- client/src/components/DashboardLayaout.jsx
+++ client/src/components/DashboardLayaout.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import Sidebar from "./SideBar";
-import Cookies from "js-cookie";
+import Cookies from "jsonwebtoken";
 
 const DashboardLayout = () => {
    const navigate = useNavigate();
--- client/src/pages/OrderBuyPage.jsx
+++ client/src/pages/OrderBuyPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Cookies from "js-cookie";
+import Cookies from "jsonwebtoken";
 import axios from "axios";
 import CreateOrderModal from "../Modals/CreateOrderBuyModal";
 import EditOrderModal from "../Modals/EditOrderBuyModal";
