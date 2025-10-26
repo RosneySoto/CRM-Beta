db39d398bfd5d35fe937b8bb7b168db5c4276b93
Jose Alvarez <rosney.sotor@gmail.com>
2025-10-26 00:51:13 -0300

    modificacion en vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE_PATH || "/CRM-Beta",
+  url: "crm-beta.onrender.com" || `preview.allowedHosts`
 })
